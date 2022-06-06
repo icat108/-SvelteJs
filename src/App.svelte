690e461761e13a3d0cf85511bb388050f601038c
icat108 <ichacnakval@gmail.com>
2022-06-06 12:27:12 +0700

    commit ke 3

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,17 @@
-<h1>Richs</h1>
+<script>
+	let title = "Richs";
+	setTimeout(() => {
+		title = "Donation";
+	}, 2000);
+</script>
+
+<h1>{title}</h1>
+
+<style>
+h1 {
+	font-size: 3em;
+	color: #e2071c;
+}
+
+
+</style>
