49f9f5bac37238bb4c67187d336c0cd1e69dd60b
icat108 <ichacnakval@gmail.com>
2022-06-27 15:35:09 +0700

    this

--- src/data/charities.js
+++ src/data/charities.js
@@ -4,7 +4,13 @@ module.exports = {
             id: 1,
             title: "Frist Charity On Project",
             category: "Money",
-            thumbnail: ""
+            thumbnail: "",
+            pledged: 0,
+            target: 10000,
+            date_end: +new Date('10 agustus 2020'),
+            profile_photo: "",
+            profile_name: "ricad",
+            no_pledges: 0
         }
     ]
 }
