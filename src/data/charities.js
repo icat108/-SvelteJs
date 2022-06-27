75cf6413a282608cb822502d855cad31323aa884
icat108 <ichacnakval@gmail.com>
2022-06-27 20:46:26 +0700

    this

--- src/data/charities.js
+++ src/data/charities.js
@@ -1,16 +1,16 @@
 module.exports = {
-    charities: [
-        {
-            id: 1,
-            title: "Frist Charity On Project",
-            category: "Money",
-            thumbnail: "",
-            pledged: 0,
-            target: 10000,
-            date_end: +new Date('10 agustus 2020'),
-            profile_photo: "",
-            profile_name: "ricad",
-            no_pledges: 0
-        }
-    ]
-}
+  charities: [
+    {
+      id: 1,
+      title: "Frist Charity On Project",
+      category: "Money",
+      thumbnail: "",
+      pledged: 10000,
+      target: 10000,
+      date_end: +new Date("10 agustus 2020"),
+      profile_photo: "",
+      profile_name: "ricad",
+      no_pledges: 0,
+    },
+  ],
+};
